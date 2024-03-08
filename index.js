7e0c6a63858719df6268b5c9043f98c440c80539
Yulii <yulikkrasulia@gmail.com>
2024-03-08 16:46:30 +0200

    add changw class for column

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const cols = document.querySelectorAll(".col")
 
+document.addEventListener("keydown", (event) =>{
+    event.preventDefault()
+    if (event.code.toLowerCase() === "space") {
+        setRandomColors()
+    }
+})
+
+document.addEventListener("click", (event) => {
+    const type = event.target.dataset.type
+    if (type === "lock") {
+        const node = event.target.tagName.toLowerCase() === "i"
+        ? event.target
+        : event.target.children[0]
+        node.classList.toggle("fa-lock-open")
+        node.classList.toggle("fa-lock")
+    }
+})
+
 function generateRandomColor() {
     const hexCodes = "0123456789ABCDEF"
     let color = ""
@@ -12,9 +30,17 @@ function generateRandomColor() {
 function setRandomColors() {
     cols.forEach((col) => {
         const text = col.querySelector("h2")
+        const btn = col.querySelector("button")
         const color = generateRandomColor()
         text.textContent = color
         col.style.background = generateRandomColor()
+        setTextColor(text, color)
+        setTextColor(btn, color)
     });
 };
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? "black" : "white"
+}
 setRandomColors();
