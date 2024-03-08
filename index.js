47f44bb51b4b8ebae5890818f8ec61aca6e2ec3f
Yulii <yulikkrasulia@gmail.com>
2024-03-08 17:29:36 +0200

    add func getColorsFromHash

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cols = document.querySelectorAll(".col")
 
-document.addEventListener("keydown", (event) =>{
+document.addEventListener("keydown", (event) => {
     event.preventDefault()
     if (event.code.toLowerCase() === "space") {
         setRandomColors()
@@ -11,11 +11,11 @@ document.addEventListener("click", (event) => {
     const type = event.target.dataset.type
     if (type === "lock") {
         const node = event.target.tagName.toLowerCase() === "i"
-        ? event.target
-        : event.target.children[0]
+            ? event.target
+            : event.target.children[0]
         node.classList.toggle("fa-lock-open")
         node.classList.toggle("fa-lock")
-    } else if (type === "copy"){
+    } else if (type === "copy") {
         copyToClickboard(event.target.textContent)
     }
 })
@@ -29,11 +29,12 @@ function generateRandomColor() {
     return "#" + color
 };
 
-function copyToClickboard (text) {
+function copyToClickboard(text) {
     navigator.clipboard.writeText(text)
 }
 
 function setRandomColors() {
+    const colors = []
     cols.forEach((col) => {
         const isLocked = col.querySelector("i").classList.contains("fa-lock")
         const text = col.querySelector("h2")
@@ -41,8 +42,11 @@ function setRandomColors() {
         const color = generateRandomColor()
 
         if (isLocked) {
+            colors.push(text.textContent)
             return
         }
+
+        colors.push(color)
 
         text.textContent = color
         col.style.background = generateRandomColor()
@@ -50,10 +54,26 @@ function setRandomColors() {
         setTextColor(text, color)
         setTextColor(btn, color)
     });
+    updateColorsHash(colors)
 };
 
 function setTextColor(text, color) {
     const luminance = chroma(color).luminance()
     text.style.color = luminance > 0.5 ? "black" : "white"
+}
+
+function updateColorsHash(colors = []) {
+    document.location.hash = colors
+        .map((col) => {
+            return col.substring(1)
+        })
+        .join("-")
+}
+
+function getColorsFromHash() {
+    if (document.location.hash.length > 1) {
+        return document.location.hash.substring(1).split("-").map(color => "#" + color)
+    }
+    return []
 }
 setRandomColors();
